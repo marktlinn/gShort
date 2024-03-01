c73c1772357abdbc00ad65384ca9715f14998c72
marktlinn <marktlinn@gmail.com>
2024-03-01 22:04:45 +0100

    add: simple link validation service

---
+++ short_svc/short.go
@@ -0,0 +1,46 @@
+package shortsvc
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+const MaxKeyLen = 16
+
+type Link struct {
+	Key string
+	URL string
+}
+
+// validateKey ensures the length of the passed Link's Key
+// is less that the MaxKeyLen
+func validateKey(k string) error {
+	if strings.TrimSpace(k) == "" {
+		return errors.New("empty key")
+	}
+	if len(k) > MaxKeyLen {
+		return fmt.Errorf("key too long: max length = %d", MaxKeyLen)
+	}
+	return nil
+}
+
+// validate the passed link, ensuring it has a Host and
+// the correct Scheme
+func validateLink(ln Link) error {
+	if err := validateKey(ln.Key); err != nil {
+		return err
+	}
+	u, err := url.ParseRequestURI(ln.URL)
+	if err != nil {
+		return err
+	}
+	if u.Host == "" {
+		return errors.New("no host")
+	}
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return errors.New("scheme must be 'HTTP' or 'HTTPS'")
+	}
+	return nil
+}
